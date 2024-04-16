9dfb4811be6c8e2961eb6a672effa23170dbbb25
Sharad <sharadritu.58.69@gmail.com>
2024-04-16 22:39:23 +0545

    add recipe card component and input field for search

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
 //import React from 'react';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useRef } from 'react';
 import './App.css';
 import * as api from './api';
 import { Recipe } from './types';
+import RecipeCard from './components/RecipeCard';
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('burgers');
-  const [recipes, setRecipes] = useState<Recipe[]>([])
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const pageNumber = useRef(1);
 
   const handleSearchSubmit = async(event: FormEvent) => {
     event.preventDefault();
     try {
       const recipes = await api.searchRecipes(searchTerm, 1);
-
       setRecipes(recipes.results);
+      pageNumber.current = 1;
     } catch (error) {
       console.log(error);
     }
   }
-
+  const handleViewMoreClick = async() => {
+    const nextPage = pageNumber.current + 1;
+    try {
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage);
+      setRecipes([...recipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
       <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <input 
+          type="text" 
+          required
+          placeholder='Enter search term ....'
+          value={searchTerm}
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        />
         <button type='submit'>Submit</button>
       </form>
 
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          recipe title: { recipe.title }
-        </div>
+        <RecipeCard recipe={recipe} />
       ))}
+
+      <button 
+        className='view-more-button'
+        onClick={handleViewMoreClick}
+      >
+        View More
+      </button>
     </div>
   )
 };
---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,18 @@
+import { Recipe } from '../types'
+
+interface Props {
+  recipe: Recipe;
+}
+
+const RecipeCard = ({recipe}: Props) => {
+  return (
+    <div className='recipe-card'>
+      <img src={recipe.image} alt="Recipe Image" />
+      <div className='recipe-card-title'>
+        <h3>{recipe.title}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default RecipeCard;
